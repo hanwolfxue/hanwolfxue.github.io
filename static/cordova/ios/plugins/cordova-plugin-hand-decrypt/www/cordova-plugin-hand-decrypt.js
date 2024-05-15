06e8db98128c4619ff66de24d2f159cd53851105
yky <945281736@qq.com>
2024-05-15 16:33:25 +0800

    使用后台模版

--- static/cordova/ios/plugins/cordova-plugin-hand-decrypt/www/cordova-plugin-hand-decrypt.js
+++
@@ -1,9 +0,0 @@
-cordova.define("cordova-plugin-hand-decrypt.cordova-plugin-hand-decrypt", function(require, exports, module) {
-var exec = require('cordova/exec');
-
-exports.decrypt = function( success, error,data) {
-    exec(success, error, "decryptCordova", "decrypt", [data]);
-};
-
-
-});
